397c63af7d0da1e4a8087be4fd0cda9744342968
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:14:00 -0300

    commit exercicio java basico 03

--- src/java_basico_03/Main.java
+++ src/java_basico_03/Main.java
@@ -1,10 +1,32 @@
 package java_basico_03;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		app.start();
 
+	}
+
+	private void start() {
+		
+		System.out.println("Digite a Idade: ");
+		Scanner scanner = new Scanner(System.in);
+		int idade = scanner.nextInt();
+		
+		if (idade > 20) {
+			 System.out.println("Idade maior que 20 ");
+		} else if (idade > 12 &&  idade < 17) {
+			System.out.println("Idade maior que 12 e menor que 17 ");
+		}else if( idade == 20 || idade < 5) {
+			System.out.println("Idade igual a 7 ou menor que 5 ");
+		}else {
+			System.out.println("Outras combinações ");
+		}
+		
 	}
 
 }
